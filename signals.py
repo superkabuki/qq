a8ec23e8e15e8c08baf003747ec7afcc114a7ea3
Super Kabuki <a@iodisco.com>
2025-05-26 11:28:25 -0400

    Added ABTSignal

--- signals.py
+++ signals.py
@@ -2,9 +2,9 @@
 Signals
 
     Signal,
-        AdBreakSignal(Signal),
-        SpliceSignal(Signal),
-        ABTSignal(Signal)
+        AdBreakSignal
+        SpliceSignal
+        ABTSignal
 
 """
 
@@ -23,7 +23,7 @@ class Signal:
 
     def decode(self, bites):
         """
-        decode base decode called with super() in subclasses,.
+        decode bytes into Signal vars.
         """
         self.startmark = bites[0:4]
         self.signal_type = hex(bites[4])
@@ -31,12 +31,12 @@ class Signal:
 
     def encode(self):
         """
-        encode base encode called with super() in subclasses
+        encode bytes into  Signal vars.
         """
         qq = b"qque"
         qq += self.int2bytes(self.signal_type, 1)
         if self.qid:
-            qq += self.hex2bytes(self.qid,2)
+            qq += self.hex2bytes(self.qid, 2)
             return qq
         print("qid required")
         return False
@@ -68,7 +68,7 @@ class Signal:
         return int(integer).to_bytes(length=howmany, byteorder="big")
 
     @staticmethod
-    def hex2bytes( hexed, howmany):
+    def hex2bytes(hexed, howmany):
         """
         hex2bytes convert hex to bytes for encoding
         """
@@ -80,6 +80,36 @@ class Signal:
         seconds2bites convert seconds to bytes for encoding
         """
         return int(seconds * NTK).to_bytes(length=howmany, byteorder="big")
+
+
+class ABTSignal(Signal):
+    """
+    ABTSignal AdBreak Terminate Signal
+    Used to return early from an Ad Break.
+
+    @ break_stops_in  a 32 bit value on a 90k clock,
+        ticks until an ad break is terminated.
+        a value of 0 indicates immediate terminate.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.break_stops_in = 0
+
+    def decode(self, bites):
+        """
+        decode bytes into ABTSignal vars.
+        """
+        super().decode(bites)
+        self.break_stops_in = self.seconds(bites[7:11])
+
+    def encode(self):
+        """
+        encode  ABTSignal vars into bytes
+        """
+        qqbase = super().encode()
+        qq = self.seconds2bytes(self.break_stops_in, 4)
+        return qqbase + qq
 
 
 class SpliceSignal(Signal):
@@ -99,7 +129,7 @@ class SpliceSignal(Signal):
 
     def decode(self, bites):
         """
-        decode decode for SpliceSignal
+        decode bytes into SpliceSignal vars.
         """
         super().decode(bites)
         self.section_length = self.int(bites[7:9])
@@ -108,7 +138,6 @@ class SpliceSignal(Signal):
         self.tier = self.hex(bites[11:13])
         self.break_duration = self.seconds(bites[13:17])
         self.compliance_flag = bites[17]
-       
         # we don't need length, pass anything left to unroll_descriptors()
         self.unroll_descriptors(bites[18:])
 
@@ -116,7 +145,7 @@ class SpliceSignal(Signal):
         """
         unroll_descriptors decode descriptors from bites
         """
-        idx=0
+        idx = 0
         bytelen = len(bites)
         while idx < bytelen:
             dtype = bites[idx]
@@ -124,20 +153,20 @@ class SpliceSignal(Signal):
             idx += 2
             dbytes = bites[idx : idx + dlength]
             idx += dlength
-            descriptor = self.descriptor_map[dtype](dbytes)
+            descriptor = descriptor_map[dtype](dbytes)
             descriptor.decode()
             self.descriptors.append(descriptor)
 
     def roll_descriptors(self):
         """
-        roll_descriptors turn descriptors into bytes for encoding
+        roll_descriptors turns descriptors into bytes for encoding
         """
 
         return b"".join([descriptor.encode() for descriptor in self.descriptors])
 
     def encode(self):
         """
-        encode encode method for SpliceSignal
+        encode SpliceSignal vars into bytes.
         """
         qqbase = super().encode()
         qq = self.hex2bytes(self.sap_type, 1)
