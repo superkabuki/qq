3e727a069ab4530c89f25b18bfb207a6e76b9c42
Super Kabuki <a@iodisco.com>
2025-05-26 03:03:38 -0400

    Signal and SpliceSignal classes

---
+++ signal.py
@@ -0,0 +1,149 @@
+"""
+Signals
+
+    Signal,
+        AdBreakSignal(Signal),
+        SpliceSignal(Signal),
+        ABTSignal(Signal)
+
+"""
+
+NTK = 90000
+
+
+class Signal:
+    """
+    Signal is the base class for AdBreakSignal, SpliceSignal and ABTSignal.
+    """
+
+    def __init__(self):
+        self.startmark = None
+        self.signal_type = None
+        self.qid = None
+
+    def decode(self, bites):
+        """
+        decode base decode called with super() in subclasses,.
+        """
+        self.startmark = bites[0:4]
+        self.signal_type = hex(bites[4])
+        self.qid = self.hex(bites[5:7])
+
+    def encode(self):
+        """
+        encode base encode called with super() in subclasses
+        """
+        qq = b"qque"
+        qq += self.int2bytes(self.signal_type, 1)
+        if self.qid:
+            qq += self.hex2bytes(self.qid,2)
+            return qq
+        print("qid required")
+        return False
+
+    def hex(self, bites):
+        """
+        hex return bites as a hex value prefixed by 0x
+        """
+        return hex(self.int(bites))
+
+    @staticmethod
+    def int(bites):
+        """
+        int return bites as an integer
+        """
+        return int.from_bytes(bites, byteorder="big")
+
+    def seconds(self, bites):
+        """
+        seconds return bites as seconds from a 90k clock
+        """
+        return self.int(bites) / NTK
+
+    @staticmethod
+    def int2bytes(integer, howmany):
+        """
+        int2bytes convert int to bytes for encoding
+        """
+        return int(integer).to_bytes(length=howmany, byteorder="big")
+
+    @staticmethod
+    def hex2bytes( hexed, howmany):
+        """
+        hex2bytes convert hex to bytes for encoding
+        """
+        return int(hexed, base=16).to_bytes(length=howmany, byteorder="big")
+
+    @staticmethod
+    def seconds2bytes(seconds, howmany):
+        """
+        seconds2bites convert seconds to bytes for encoding
+        """
+        return int(seconds * NTK).to_bytes(length=howmany, byteorder="big")
+
+
+class SpliceSignal(Signal):
+    """
+    SpliceSignal Class
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.section_length = 0
+        self.sap_type = None
+        self.cw_index = None
+        self.tier = None
+        self.break_duration = 0
+        self.compliance_flag = None
+        self.descriptors = []
+
+    def decode(self, bites):
+        """
+        decode decode for SpliceSignal
+        """
+        super().decode(bites)
+        self.section_length = self.int(bites[7:9])
+        self.sap_type = self.hex(bites[9])
+        self.cw_index = self.hex(bites[10])
+        self.tier = self.hex(bites[11:13])
+        self.break_duration = self.seconds(bites[13:17])
+        self.compliance_flag = bites[17]
+       
+        # we don't need length, pass anything left to unroll_descriptors()
+        self.unroll_descriptors(bites[18:])
+
+    def unroll_descriptors(self, bites):
+        """
+        unroll_descriptors decode descriptors from bites
+        """
+        idx=0
+        bytelen = len(bites)
+        while idx < bytelen:
+            dtype = bites[idx]
+            dlength = bites[idx + 1]
+            idx += 2
+            dbytes = bites[idx : idx + dlength]
+            idx += dlength
+            descriptor = self.descriptor_map[dtype](dbytes)
+            descriptor.decode()
+            self.descriptors.append(descriptor)
+
+    def roll_descriptors(self):
+        """
+        roll_descriptors turn descriptors into bytes for encoding
+        """
+
+        return b"".join([descriptor.encode() for descriptor in self.descriptors])
+
+    def encode(self):
+        """
+        encode encode method for SpliceSignal
+        """
+        qqbase = super().encode()
+        qq = self.hex2bytes(self.sap_type, 1)
+        qq += self.hex2bytes(self.cw_index, 1)
+        qq += self.hex2bytes(self.tier, 1)
+        qq += self.seconds2bytes(self.break_duration, 4)
+        qq += self.int2bytes(self.compliance_flag, 1)
+        qq += self.roll_descriptors()
+        return qqbase + self.int2bytes(self.section_length, 2) + qq
